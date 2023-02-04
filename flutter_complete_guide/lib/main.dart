4c2bca019ddcfb9e802348933db3aba6e302613d
IvanB <ivar110kg@gmail.com>
2023-02-04 17:00:30 +0400

    Add main

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,20 +1,13 @@
-double addNumber(double d1, double d2) {
-  return (d1 / d2);
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
 }
 
-void main(List<String> args) {
-  print('We have ${args.length} args');
-  var hui = addNumber(12.3, 13.2);
-  hui = addNumber(13.3, 1.2);
-  print(hui);
-  Person person = Person();
-  print(person.show());
-}
-
-class Person {
-  String name = '';
-  int age = 0;
-  String show() {
-    return 'name:$name\nage:$age';
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext contex) {
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
   }
 }

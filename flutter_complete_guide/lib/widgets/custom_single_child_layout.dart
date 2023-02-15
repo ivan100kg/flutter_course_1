3a08b49483d4ac20a6b843fbf88ba017ea504621
IvanB <ivar110kg@gmail.com>
2023-02-15 17:40:38 +0400

    Add custom layout

--- flutter_complete_guide/lib/widgets/custom_single_child_layout.dart
+++ flutter_complete_guide/lib/widgets/custom_single_child_layout.dart
@@ -8,13 +8,25 @@ class MyCustomSingle extends StatelessWidget {
     return Center(
       child: Container(
         color: Colors.amber,
-        child: CustomSingleChildLayout(
-          delegate: OwnSingleChildLayoutDelegate(),
-          child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.brown,
-          ),
+        child: CustomMultiChildLayout(
+          delegate: MyMultiDelegate(),
+          children: [
+            LayoutId(
+              id: 1,
+              child: Text(
+                  'data-left-000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'),
+            ),
+            LayoutId(
+              id: 2,
+              child: Text(
+                  'data-midle-9999999999999999999999999999999999999999999999999'),
+            ),
+            LayoutId(
+              id: 3,
+              child: Text(
+                  'data-right-4444444444444444444444444444444444444444444444444444444444444444444'),
+            ),
+          ],
         ),
       ),
     );
@@ -38,4 +50,25 @@ class OwnSingleChildLayoutDelegate extends SingleChildLayoutDelegate {
 
   @override
   Offset getPositionForChild(Size size, Size childSize) => Offset.zero;
+}
+
+class MyMultiDelegate extends MultiChildLayoutDelegate {
+  @override
+  void performLayout(Size size) {
+    if (hasChild(1) && hasChild(2) && hasChild(3)) {
+      layoutChild(
+          1, BoxConstraints.loose(Size(size.width * 2 / 3, size.height)));
+      layoutChild(2, BoxConstraints.tight(Size(size.width / 6, size.height)));
+      layoutChild(3, BoxConstraints.loose(Size(size.width / 6, size.height)));
+      positionChild(1, Offset(0, size.height / 2));
+      positionChild(2, Offset(size.width * 2 / 3, size.height * 1 / 2));
+      positionChild(
+          3, Offset(size.width - size.width * 1 / 6, size.height * 1 / 2));
+    }
+  }
+
+  @override
+  bool shouldRelayout(covariant MultiChildLayoutDelegate oldDelegate) {
+    return true;
+  }
 }

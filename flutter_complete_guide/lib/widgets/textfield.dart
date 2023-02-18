195dd2dd000e7143b1bfa0b553c6d39c848906eb
IvanB <ivar110kg@gmail.com>
2023-02-18 12:23:10 +0400

    Add root Text Field settings

--- flutter_complete_guide/lib/widgets/textfield.dart
+++ flutter_complete_guide/lib/widgets/textfield.dart
@@ -7,47 +7,74 @@ class MyTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: TextField(
-          decoration: InputDecoration(
-            enabled: true,
-            errorText: null,
-            icon: Icon(Icons.access_time),
-            border: OutlineInputBorder(),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.green,
-                width: 5,
+    return Column(
+      children: [
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              textInputAction: TextInputAction.next,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                enabled: true,
+                errorText: null,
+                icon: Icon(Icons.access_time),
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.green,
+                    width: 5,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(50),
+                  ),
+                ),
+                labelText: 'Hello',
+                labelStyle: TextStyle(
+                  color: Colors.deepOrange,
+                ),
+                helperText: 'Fuck you',
+                helperStyle: TextStyle(),
+                helperMaxLines: 1,
+                hintText: 'huohuohuou',
+                hintStyle: TextStyle(
+                  color: Colors.black12,
+                ),
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                prefixIcon: Icon(Icons.money),
+                prefix: Text('pre: '),
+                isCollapsed: true,
+                contentPadding: EdgeInsets.all(30),
+                filled: true,
+                fillColor: Colors.amber,
+                focusColor: Colors.black26,
+                hoverColor: Colors.black,
               ),
-              borderRadius: BorderRadius.all(
-                Radius.circular(50),
+              enableSuggestions: true,
+              textCapitalization: TextCapitalization.characters,
+              style: TextStyle(
+                fontSize: 30,
               ),
+              maxLines: 2,
+              minLines: 2,
+              maxLength: 100,
+              textAlign: TextAlign.center,
             ),
-            labelText: 'Hello',
-            labelStyle: TextStyle(
-              color: Colors.deepOrange,
-            ),
-            helperText: 'Fuck you',
-            helperStyle: TextStyle(),
-            helperMaxLines: 1,
-            hintText: 'huohuohuou',
-            hintStyle: TextStyle(
-              color: Colors.black12,
-            ),
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            prefixIcon: Icon(Icons.money),
-            prefix: Text('pre: '),
-            isCollapsed: true,
-            contentPadding: EdgeInsets.all(30),
-            filled: true,
-            fillColor: Colors.amber,
-            focusColor: Colors.black26,
-            hoverColor: Colors.black,
           ),
         ),
-      ),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              textInputAction: TextInputAction.next,
+              obscureText: true,
+              obscuringCharacter: '#',
+              autocorrect: false,
+              enableSuggestions: false,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
